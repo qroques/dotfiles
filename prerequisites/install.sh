e083fc1a92e50aa459803d9679960225d239a63b
Quentin Roques <quentin.roques@knplabs.com>
2025-02-18 11:20:09 +0100

    fix: dotfile folder

--- prerequisites/install.sh
+++ prerequisites/install.sh
@@ -4,6 +4,8 @@ sudo apt update
 sudo apt -y install git
 sudo apt -y install make
 
+# Move existing dotfiles if exists
+[ ! -d ~/dotfiles ] || mv ~/dotfiles ~/dotfile-bakup-$(date '+%Y-%m-%d')
 git clone https://github.com/qroques/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 make install
